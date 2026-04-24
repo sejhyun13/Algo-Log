5c17356262f840e5d275675308a6f7856530bbfe
Seungjun Hyun <sejhyun13@gmail.com>
2026-04-24 09:12:28 +0900

    [D4] Title: MST 만들기, Time: 159 ms, Memory: 62,464 KB -BaekjoonHub

---
+++ SWEA/D4/26504. MST 만들기/MST 만들기.py
@@ -0,0 +1,16 @@
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    weights = list(map(int,input().split()))
+    M = len(weights)
+    weights.sort()
+    minimum = sum(weights[0:N-1])
+    # 최소 스패닝 트리 최대비용 구하기
+    maximum = 0
+    i = 0
+    add = 1
+    while i < len(weights):
+        maximum += weights[i]
+        i += add
+        add += 1
+    print(minimum, maximum)
